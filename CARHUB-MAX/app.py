cabba08269101b7d5da9c5933584cf7dbf50761e
Omewah <joelomewah@ymail.com>
2024-03-27 06:14:24 +0100

    [Update]: Enable user login authentication and new user registration

--- CARHUB-MAX/app.py
+++ CARHUB-MAX/app.py
@@ -1,26 +1,16 @@
+from flask import Flask
 from flask import Flask, flash, redirect, url_for, render_template, request, session
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import sessionmaker
+from tabledef import *
 import os
+
+engine = create_engine('sqlite:///carhubmax.db', echo=True)
+Session = sessionmaker(bind=engine)
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
-
-# Prints the current working directory
-print("Current Working Directory:", os.getcwd())
-
-# Configure SQLite URI
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-
-
-# SQL database user class model
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(250), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)
-
-    # The DB table name
-    __tablename__ = 'dacarhub_user_table'
 
 
 @app.route('/', endpoint='index')
@@ -71,19 +61,27 @@ def booking_6():
 @app.route('/regist', methods=['GET', 'POST'])
 def regist():
     if request.method == 'POST':
-        email = request.form['username']
+        username = request.form['username']
         password = request.form['password']
         confirm_password = request.form['confirm_password']
 
+        # Checks if the registration passwords match
         if password != confirm_password:
-            flash('Password and Confirm Password must match', 'danger')
-        else:
-            # this will create the new user objeect in the DB
-            new_user = User(email=email, password=password)
-            db.session.add(new_user)
-            db.session.commit()
-            flash('Registration successful! You can now log in.', 'success')
-            return redirect(url_for('index'))
+            flash('Password and Confirm Password must match', 'error')
+            return redirect(url_for('regist'))
+
+        # Checks if the username is already taken in the existing database
+        session = Session()
+        if session.query(User).filter_by(username=username).first():
+            flash('Username already exists. Please choose a different one.', 'error')
+            return redirect(url_for('regist'))
+
+        # Creates a new user and add it to the database
+        new_user = User(username=username, password=password)
+        session.add(new_user)
+        session.commit()
+        flash('Registration successful! You can now log in.', 'success')
+        return redirect(url_for('login'))
 
     return render_template('regist.html')
 
@@ -91,15 +89,22 @@ def regist():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        if request.form['password'] == 'password' and request.form['username'] == 'admin':
+        POST_USERNAME = str(request.form['username'])
+        POST_PASSWORD = str(request.form['password'])
+
+        Session = sessionmaker(bind=engine)
+        s = Session()
+        query = s.query(User).filter(User.username.in_([POST_USERNAME]), User.password.in_([POST_PASSWORD]))
+        result = query.first()
+        if result:
             session['logged_in'] = True
             return redirect(url_for('index'))
         else:
-            flash('Wrong password!')
-            return render_template('login.html')
-    else:
-        return render_template('login.html')
+            flash('Wrong password!', 'error')
+
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
+    app.secret_key = os.urandom(12)
     app.run(host='0.0.0.0', port=5000)
