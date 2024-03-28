b852b4871a150e2d820b2b00ae673d5792c22b10
Omewah <joelomewah@ymail.com>
2024-03-28 22:42:23 +0100

    [Update]: modify the booking_1 route the connect the booking data to the database

--- CARHUB-MAX/app.py
+++ CARHUB-MAX/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
-from flask import Flask, flash, redirect, url_for, render_template, request, session
+from flask import Flask, flash, redirect
+from flask import url_for, render_template, request, session
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import sessionmaker
 from tabledef import *
@@ -38,11 +39,6 @@ def about():
     return render_template('about.html')
 
 
-@app.route('/booking_1')
-def booking_1():
-    return render_template('booking_1.html')
-
-
 @app.route('/booking_2')
 def booking_2():
     return render_template('booking_2.html')
@@ -73,7 +69,7 @@ def regist():
         # Checks if the username is already taken in the existing database
         session = Session()
         if session.query(User).filter_by(username=username).first():
-            flash('Username already exists. Please choose a different one.', 'error')
+            flash('Failed! Username already exists.', 'error')
             return redirect(url_for('regist'))
 
         # Creates a new user and add it to the database
@@ -103,6 +99,53 @@ def login():
             flash('Wrong password!', 'error')
 
     return render_template('login.html')
+
+
+@app.route('/booking_1', methods=['GET', 'POST'])
+def booking_1():
+    if request.method == 'POST':
+        first_name = request.form['first_name']
+        last_name = request.form['last_name']
+        email = request.form['email']
+        mobile_number = request.form['mobile_number']
+        pickup_location = request.form['pickup_location']
+        drop_location = request.form['drop_location']
+        pickup_date = request.form['pickup_date']
+        pickup_time = request.form['pickup_time']
+        adult = request.form['adult']
+        child = request.form['child']
+        special_request = request.form['special_request']
+        payment = request.form['payment']
+
+        session = Session()
+        new_booking = CarBooking(
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            mobile_number=mobile_number,
+            pickup_location=pickup_location,
+            drop_location=drop_location,
+            pickup_date=pickup_date,
+            pickup_time=pickup_time,
+            adult=adult,
+            child=child,
+            special_request=special_request,
+            payment=payment
+        )
+            
+        session.add(new_booking)
+        session.commit()
+        session.close()
+
+        flash('Successful! Thank you for choosing CARHUB-MAX.', 'success')
+        return redirect(url_for('confirmation'))
+    
+    return render_template('booking_1.html')
+
+
+@app.route('/confirmation')
+def confirmation():
+    return render_template('confirmation.html')
 
 
 if __name__ == '__main__':
